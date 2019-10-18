0cf8e2e73eedc4881c42ead9e03f70f532bc9eba
fdz23 <ferdavid_9@hotmail.com>
2019-10-18 16:52:20 -0300

    Add files via upload
    Adicionada uma pagina servida na porta 80, ainda nao e a versao final

--- SGEmbarcado.ino
+++ SGEmbarcado.ino
@@ -1,32 +1,74 @@
 /*
- * SISTEMA DE GESTAO DE EMBARCADOS
- * 
- * DESCRICAO: cadastrar, excluir, atualizar e localizar as informacoes sobre o embarcado,
- * rede, sensores e atuadores utilizando a plataforma ESP32 da Espressif.
- * 
- */
+   SISTEMA DE GESTAO DE EMBARCADOS
+
+   DESCRICAO: cadastrar, excluir, atualizar e localizar as informacoes sobre o embarcado,
+   rede, sensores e atuadores utilizando a plataforma ESP32 da Espressif.
+
+*/
 
 #include "configuration.h"
 
+AsyncWebServer server(80);
+void notFound(AsyncWebServerRequest * request) {
+  request->send(404, "text/plain", "Not found");
+}
 char e;
 
 void setup() {
   Serial.begin(115200);
+  //loadFile("/config.json");
   net.toConnected();
+  //net.toPrintNetwork();
   //net.toAccessPoint();
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+  }
+  else {
+    Serial.println("SPIFFS Mount successful!");
+  }
+
+  File file = SPIFFS.open("/index.html", FILE_READ);
+
+  if (!file) {
+    Serial.println("[  Fail \t] - Falha ao carregar o arquivo de configJSON !!");
+  }
+  else {
+    Serial.println("SPIFFS Load successful!");
+  }
+
+  server.on("/embarcado/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/embarcado.html", "text/html");
+  });
+
+  server.on("/embarcado/get", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    String hardware, firmware, location, ip, macAndressa, modo, mask, gateway, essid;
+    //posso usar essas variaveis para colocar elas em qualquer lugar
+    hardware = request->getParam("hardware")->value();
+    firmware = request->getParam("firmware")->value();
+    location = request->getParam("location")->value();
+    ip = request->getParam("ip")->value();
+    macAndressa = request->getParam("macAddress")->value();
+    modo = request->getParam("modo")->value();
+    mask = request->getParam("mask")->value();
+    gateway = request->getParam("gateway")->value();
+    essid = request->getParam("essid")->value();
+  });
+
+  server.onNotFound(notFound);
+
+  server.begin();
 }
 
 void loop() {
-  
-  net.toPrintNetwork();   
-  for(;;){
-    while (Serial.available()>0){
-          e = Serial.read(); // Lê byte do buffer serial;
-          Serial.print(e); // Faz o eco do byte recebido;
-          if (e == 'a')
-              embedded.toRestart();
-          }
-        }
-    
+  for (;;) {
+    //    while (Serial.available()>0){
+    //          e = Serial.read(); // Lê byte do buffer serial;
+    //          Serial.print(e); // Faz o eco do byte recebido;
+    //          if (e == 'a')
+    //              embedded.toRestart();
+    //          }
+  }
+
 
 }
--- boarded.h
+++ boarded.h
@@ -1,22 +1,45 @@
 #ifndef BOARDED_H
 #define BOARDED_H
 
-class Boarded{
+class Boarded {
   private:
     int id;
-    String marca;
-    String descHardware;
-    String firmware;
-    String location;
-    
+    const char* marca;
+    const char* descHardware;
+    const char* firmware;
+    const char* location;
+
   public:
-    Boarded(){}
-    void toSetup(){}
-    void toRestart(){
-        Serial.println("Restartando o sistema embarcado ...");
-        ESP.restart();
-      }
-    void toRegiterLog(){}
+    Boarded() {}
+    void toSetup() {}
+    void toRestart() {
+      Serial.println("Restartando o sistema embarcado ...");
+      ESP.restart();
+    }
+    void toRegiterLog() {}
+    void loadBoarded(JsonDocument obj) {
+      this->marca = obj["boarded"]["brand"].as<char*>();
+      this->descHardware = obj["boarded"]["descHardware"].as<char*>();
+      this->firmware = obj["boarded"]["firmware"].as<char*>();
+      this->location = obj["boarded"]["location"].as<char*>();
+    }
+    void saveBoarded(JsonDocument obj) {
+      obj["boarded"]["brand"].set(this->marca);
+      obj["boarded"]["descHardware"].set(this->descHardware);
+      obj["boarded"]["firmware"].set(this->firmware);
+      obj["boarded"]["location"].set(this->location);
+    }
+    void toPrintBoarded() {
+      Serial.println("\n\nBoarded :::");
+      Serial.print("Marca: ");
+      Serial.println(this->marca);
+      Serial.print("descHardware: ");
+      Serial.println(this->descHardware);
+      Serial.print("firmware: ");
+      Serial.println(this->firmware);
+      Serial.print("location: ");
+      Serial.println(this->location);
+    }
 };
 
 #endif
